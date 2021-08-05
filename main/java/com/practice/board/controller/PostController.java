35ade31df1c79df1ff09bdcda1dc90bac519a2a3
여지원 <yooara0508@naver.com>
2021-08-05 15:44:01 +0900

    넘버링 문제 해결 이제 게시글 리스트에서 댓글 몇 개인지 보이도록

--- main/java/com/practice/board/controller/PostController.java
+++ main/java/com/practice/board/controller/PostController.java
@@ -42,12 +42,18 @@ public class PostController {
     @ApiOperation(value="게시판 화면 출력", notes="post의 id의 역순으로 게시물 5개씩 가져옴")
     public String pageList(@RequestParam(value = "page", defaultValue ="1") int page, Model model) {
         int start = 1;
-        if(page > 10){
-            start = (page/10)*10+1;
+        if(page >= 10){
+            if(page % 10 == 0){
+                start = (page/10-1)*10+1;
+            }
+            else{
+                start = (page/10)*10+1;
+            }
         }
 
         int max = postService.pages();
         int pages = start+9;
+
         if(pages>max) pages=max;
 
 
